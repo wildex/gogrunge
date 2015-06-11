d99aa01bce6c0af91bcfa77dd65de72d7cd5c8dc
Andrii Alieksieienko <andrii.alieksieienko@aeriagames.com>
2015-06-11 22:15:30 +0200

    added main template handler

--- server.go
+++ server.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "net"
+    "net/http"
+    "net/http/fcgi"
+    "html/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Gru grungee", r.URL.Path[1:])
+type FastCGIServer struct{}
+
+type Msg struct {
+    Id int
+    Name string
+    Message string
+}
+
+func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+    t := template.New("index.html") //create a new template
+    t, _ = t.ParseFiles("templates/index.html") //open and parse a template text file
+
+    messages := []Msg{Msg{1, "test", "test12"}, Msg{2, "test2", "test23"}}
+    t.Execute(resp, messages)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+    listener, _ := net.Listen("tcp", "127.0.0.1:9001")
+    srv := new(FastCGIServer)
+    fcgi.Serve(listener, srv)
 }
